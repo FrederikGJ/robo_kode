0036e1636dbbb5aee62d05b1b3a5c8e4539a0837
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2024-06-12 12:31:09 +0200

    blynk app

--- blynk_app/blynk_app.ino
+++ blynk_app/blynk_app.ino
@@ -24,11 +24,14 @@ const long intervalServo = 1500;  // Interval for servo movement (in millisecond
 bool motorRunning = false;  // Variable to store motor state (false = off, true = on)
 bool servoMovingForward = true;  // Variable to determine servo direction (true = forward, false = backward)
 
+// Authentication token
+char auth[] = "KC7dMzJcI8w_CnAZxe_b9qJM_kIyiMYI";
+
 void setup() {
   // Debug console
   Serial.begin(9600);
 
-  Blynk.begin(BLYNK_TEMPLATE_ID, WIFI_SSID, WIFI_PASS);
+  Blynk.begin(auth, WIFI_SSID, WIFI_PASS);
 
   // Set all motor controller pins to output
   pinMode(ENA, OUTPUT);
@@ -69,13 +72,36 @@ void loop() {
 BLYNK_WRITE(V1) {
   int value = param.asInt();
   if (value == 1) {
-    // Turn on the motors
+    // Turn on the motors (forward)
     digitalWrite(ENA, HIGH);
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
     digitalWrite(ENB, HIGH);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
+  } else {
+    // Turn off the motors
+    digitalWrite(ENA, LOW);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(ENB, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+  motorRunning = !motorRunning;  // Toggle motor state
+}
+
+// Function to control the motor backward
+BLYNK_WRITE(V2) {
+  int value = param.asInt();
+  if (value == 1) {
+    // Turn on the motors (backward)
+    digitalWrite(ENA, HIGH);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(ENB, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
   } else {
     // Turn off the motors
     digitalWrite(ENA, LOW);
